fe8409b5322d59745a68db8ee4191dd941df45f7
Rock Zhang <iamgtskk@gmail.com>
2014-09-25 11:01:04 +0800

    not supporting php

--- application/libraries/MY_Session.php
+++
@@ -1,9 +0,0 @@
-<?php
-class MY_Session extends CI_Session{
-	function sess_update(){
-		// Listen to X_HTTP_REQUESTED_WITH
-		if(isset($_SERVER['X_HTTP_REQUESTED_WITH']) && $_SERVER['X_HTTP_REQUESTED_WITH'] !== 'XMLHttpRequest'){
-			parent::sess_update();
-		}
-	}
-}
--- application/views/components/sidebar.php
+++
@@ -1,4 +0,0 @@
-<div class="span3" id="sidebar">
-	<h2>最近消息</h2>
-	<?php echo article_links($recent_news);?>
-</div>
