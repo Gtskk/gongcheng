0d85aa080e9e462c2b9506e68cf367bb575e00c1
Rock Zhang <iamgtskk@gmail.com>
2014-12-03 17:19:41 +0800

    添加datepicker插件

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -14,6 +14,7 @@
 	    <script src="<?php echo base_url('js/admin.js');?>"></script>
 	    <script src="<?php echo base_url('js/bootstrap-datetimepicker.min.js');?>"></script>
 	    <script src="<?php echo base_url('js/bootstrap-datetimepicker.zh-CN.js');?>"></script>
+	    <script src="<?php echo base_url('js/bootstrap-datepicker.js');?>"></script>
 	   	<?php if(isset($sortable) && $sortable):?>
 	    <script src="<?php echo base_url('js/jquery-ui-1.9.1.custom.min.js');?>"></script>
         <script src="<?php echo base_url('js/jquery.mjs.nestedSortable.js');?>"></script>
